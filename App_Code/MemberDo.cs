c9d21eed83e42c0961b8f306c5385bb0a961feaf
andomang <dksehvkf1206@naver.com>
2026-04-03 17:43:05 +0900

    Add files via upload

--- App_Code/MemberDo.cs
+++ App_Code/MemberDo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-/// <summary>
+﻿/// <summary>
 /// MemberDo의 요약 설명입니다.
 /// </summary>
 public class MemberDo
